1c062f2da276e07ff722f0bbee879f8519bbe221
songjia <songjia@126.com>
2016-05-30 08:51:24 +0800

    修改预测算法

--- src/main/java/yuceHandler/App.java
+++ src/main/java/yuceHandler/App.java
@@ -77,9 +77,9 @@ public class App {
 		String maxIssueNumber = data2Db.findMaxIssueIdFromSrcDb();
 		if(!maxIssueId.equals(maxIssueNumber)){
 			maxIssueId = maxIssueNumber;
-			//startDanMa(maxIssueId);                //胆码计算放发组开始
+			startDanMa(maxIssueId);                //胆码计算放发组开始
 			// 四码复式计算组  
-			//startSiMa(maxIssueId);
+			startSiMa(maxIssueId);
 			//同码预测
 	   		startSameNumber(maxIssueId);
 		}
